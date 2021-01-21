8431ff3556379cc762331654f46b73577f7a06ac
HopenLau <lauhokfan@163.com>
2021-01-21 11:01:06 +0800

    upload project source

---
+++ src/com/mytest/servlet/TestMain01.java
@@ -0,0 +1,12 @@
+package com.mytest.servlet;
+
+import java.time.LocalDateTime;
+
+public class TestMain01 {
+
+	public static void main(String[] args) {
+		System.out.println("LocalDateTime.now() -->" + LocalDateTime.now() + "<--");
+
+	}
+
+}
